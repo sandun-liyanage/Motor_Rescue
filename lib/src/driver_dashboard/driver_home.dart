ebc39ca324e6d766598a10d834132bb0da51f3fa
Sandun Liyanage <sandunudesh03@gmail.com>
2023-03-04 17:56:44 +0530

    driver dashboard

--- lib/src/driver_dashboard/driver_home.dart
+++ lib/src/driver_dashboard/driver_home.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
 
 class DriverHome extends StatefulWidget {
@@ -18,39 +20,100 @@ class _DriverHomeState extends State<DriverHome> {
         toolbarHeight: 75,
         leadingWidth: 75,
       ),
-      body: Material(
-        //elevation: 20,
-        //borderRadius: BorderRadius.all(Radius.circular(30)),
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(30),
-              image: DecorationImage(
-                  image: AssetImage('assets/images/mechanic.png')),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.1),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: ConstrainedBox(
+                constraints: BoxConstraints(maxWidth: 300),
+                child: Text(
+                  'Hello... Welcome back Sandun',
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Container(
+              height: 400,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(30),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.blueGrey.withOpacity(0.7),
 
-                  blurRadius: 4,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
+                    blurRadius: 10,
+                    //offset: Offset(0, 0), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    height: 300,
+                    width: 500,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                      image: const DecorationImage(
+                          image: AssetImage('assets/images/getAssistance.jpg'),
+                          fit: BoxFit.cover),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.blueGrey.withOpacity(0.5),
+
+                          blurRadius: 10,
+                          //offset: Offset(0, 0), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                    //child: Image(image: AssetImage('assets/images/vBreakdown.png')),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: Row(
+                      children: [
+                        Flexible(
+                          child: Text(
+                            'best mechanics in your area are ready help you...',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                        ),
+                        SizedBox(width: 10),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              textStyle: TextStyle(
+                                  fontSize: 22, fontWeight: FontWeight.bold),
+                              padding: EdgeInsets.symmetric(
+                                horizontal: 20,
+                                vertical: 15,
+                              ),
+                              backgroundColor: Colors.white,
+                              foregroundColor: Colors.blue,
+                              side: BorderSide(width: 3, color: Colors.blue),
+                              elevation: 15),
+                          child: Text('Get Assistance'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            child: Stack(
-              children: const [
-                Image(
-                  image: AssetImage('assets/images/mechanic.png'),
-                  height: 50,
-                ),
-                Text(
-                  'Having ussues with your vehicle?',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 50),
-                ),
-              ],
+            SizedBox(height: 20),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                'Tips',
+                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
